503a2ff90941736d5d05b7ce918a3435a66c3be4
jeferson7770 <jefersonmoreira770@gmail.com>
2025-10-02 13:02:52 -0300

    Configuração do projeto com Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,39 +3,26 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-// Minificar e copiar scripts
 function scripts(){
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
+        .pipe(gulp.dest('./dist/js'))
 }
 
-// Compilar SCSS em CSS minificado
 function styles(){
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-// Otimizar imagens
 function images(){
     return gulp.src('./src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/img'));
 }
 
-// Copiar HTML da raiz para dist
-function html() {
-    return gulp.src('./*.html') // pega index.html, login.html, register.html
-        .pipe(gulp.dest('./dist'));
-}
-
-// Task padrão (build completo)
-exports.default = gulp.parallel(styles, images, scripts, html);
-
-// Modo watch (desenvolvimento)
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-    gulp.watch('./*.html', gulp.parallel(html));
-};
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
